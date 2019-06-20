7d053c151476eac4a3a5130d96514efb42427e16
Irina Lepekhina <irina.lepekhina95@gmail.com>
2019-06-20 17:36:19 +0300

    functions

--- src/index.js
+++ src/index.js
@@ -6,3 +6,31 @@ document.getElementById("app").innerHTML = `
   for more info about Parcel.
 </div>
 `;
+
+//Function Declaration
+// Hoisted
+// function sayHi() {
+//   return console.log('hi');
+// };
+
+//Function Expression
+//Anon, not named
+//Not hoisting
+//Passed around or used
+// const sayHi = function() {
+//   return console.log('hi');
+// };
+
+//Named function
+// const sayHi = function sayHi() {
+//   return console.log('hi');
+// };
+
+// const sayHi = () => {
+//   return console.log('hi');
+// };
+
+// const sayHi = () => (console.log('hi'));
+const sayHi = () => console.log('hi');
+
+sayHi();
