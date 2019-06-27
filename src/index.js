1b4ec59ab58fca9a832cd36a8c2195feebb12a3f
Irina Lepekhina <irina.lepekhina95@gmail.com>
2019-06-27 17:58:37 +0300

    js and the dom part 3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-const logo = document.querySelector('#logo');
-logo.innerText = 'YO!';
+//Generate random color
+//RGB 0-255
+//Apply to DOM
+//Update color on event
 
-const heading = document.createElement('h1');
-heading.innerHTML = '<span>Hello</span>' + document.URL;
-document.body.appendChild(heading);
+const generateColorValue = () => Math.floor(Math.random() * 256);
+
+const createColor = () => {
+  const red = generateColorValue();
+  const green = generateColorValue();
+  const blue = generateColorValue();
+  return `rgb(${red}, ${green}, ${blue})`
+};
+
+const applyColorToBody = color => {
+  return document.body.style.backgroundColor = color;
+};
+
+const addRandomColorToBg = () => {
+  const color = createColor();
+  return applyColorToBody(color);
+};
+
+console.log(addRandomColorToBg());
