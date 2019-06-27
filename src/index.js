224994d9a78c8e468d7bd5fefba40a61214626ec
Irina Lepekhina <irina.lepekhina95@gmail.com>
2019-06-27 17:23:08 +0300

    js and the dom + mdn docs

--- src/index.js
+++ src/index.js
@@ -7,29 +7,8 @@ document.getElementById("app").innerHTML = `
 </div>
 `;
 
-const cart = [10, 5, 15];
+const heading = document.createElement('h1');
+heading.innerHTML = '<span>Hello</span>' + document.URL;
+document.body.appendChild(heading);
 
-const SHIPPING_COST = 10;
-
-const fakeAPICharge = total => true;
-const fakeSendRecipt = total => true;
-
-const getSubTotal = cart => cart.reduce((tempTotal, item) => tempTotal + item);
-const getTotal = subTotal => subTotal + SHIPPING_COST;
-const sendRecipt = ({ email, total }) => 
-  fakeSendRecipt({
-    email,
-    total
-  });
-
-const checkout = cart => {
-  const subTotal = getSubTotal(cart);
-  const total = getTotal(subTotal);
-  const orderSuccess = fakeAPICharge(total);
-  if (orderSuccess) {
-    sendRecipt({ email: 'fakeemail@gmail.com', total })
-  }
-  return orderSuccess;
-};
-
-checkout(cart);
+console.log(document.URL);
