b37d09ccd91f5d038a7bc307f89cf8a4dc69ef62
Irina Lepekhina <irina.lepekhina95@gmail.com>
2019-07-02 16:25:42 +0300

    async await

--- src/index.js
+++ src/index.js
@@ -3,18 +3,24 @@
 const chargeCard = () => 
   new Promise((resolve, reject) => {
     //card is valid
-    reject('fail!!!');
+    // reject('fail!!!');
     if (true) {
       return resolve(true);
     }
     return reject(false);
   });
 
-chargeCard().then((val) => {
-  console.log(val);
-}).catch((err) => {
-  console.log(err);
-});
+const chargeCC = async (ccNumber) => {
+  try {
+    const result = await chargeCard(ccNumber);
+    console.log(result);
+    return result;
+  } catch(error) {
+    console.log(error);
+  }
+};
+
+console.log(chargeCC('23412341234'));
 
 //Callback hell
 // chargeCard('123412341234', (res) => {
