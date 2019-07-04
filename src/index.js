c9a273f3c2820082680b322da77d3af6710a10f4
Irina Lepekhina <irina.lepekhina95@gmail.com>
2019-07-04 17:31:35 +0300

    async iife

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-//FETCH!
+(async () => {
+  try {
+    const SEARCH_QUERY = 'Javascript';
+    const SEARCH_URL = 'https://openlibrary.org/search.json?q=';
+    const API_URL =   `${SEARCH_URL}${SEARCH_QUERY}`
 
-const SEARCH_QUERY = 'Javascript';
-const SEARCH_URL = 'https://openlibrary.org/search.json?q=';
-const API_URL =   `${SEARCH_URL}${SEARCH_QUERY}`
+    const res = await fetch(API_URL);
+    const data = await res.json();
 
-fetch(API_URL)
-  .then((res) => {
-    return res.json();
-  })
-  .then(data => {
     console.log(data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+  } catch (error) {
+    console.error(error.message);
+  }
+})();
