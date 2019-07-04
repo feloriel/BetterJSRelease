6e0c80b028418e95f4642b123d57e8d0e87c38fa
Irina Lepekhina <irina.lepekhina95@gmail.com>
2019-07-04 17:17:39 +0300

    so fetch

--- src/index.js
+++ src/index.js
@@ -1,41 +1,16 @@
-class Team {
-  constructor(name) {
-    this.name = name;
-  }
+//FETCH!
 
-  celebrate() {
-    console.log('Lets dance');
-  }
-}
+const SEARCH_QUERY = 'Javascript';
+const SEARCH_URL = 'https://openlibrary.org/search.json?q=';
+const API_URL =   `${SEARCH_URL}${SEARCH_QUERY}`
 
-class HockeyTeam extends Team {
-  constructor(name) {
-    super(name);
-    this.type = 'Hockey';
-  }
-
-  scoreGoal() {
-    console.log('he shoots, he scores');
-  }
-}
-
-class FootballTeam extends Team {
-  constructor(name) {
-    super(name);
-    this.type = 'Football';
-  }
-
-  touchdown() {
-    console.log('Go for two!');
-  }
-}
-
-const wings = new HockeyTeam('Red Wings');
-const lions = new FootballTeam('Lions');
-
-wings.scoreGoal();
-lions.touchdown();
-wings.celebrate();
-lions.celebrate();
-
-console.log(wings, lions);
+fetch(API_URL)
+  .then((res) => {
+    return res.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
